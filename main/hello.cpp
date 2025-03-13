05faf532d971d6b15a03dafd427a1dd0559e7d5f
meghnaroyal <manthurimeghana@gmail.com>
2025-03-13 22:16:00 +0530

    updated with small change

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this modified  \n";
+  cout << "I have successfully built and run this modified again  \n";
   return 0;
 }
