ae30fb95e44e80fb295b9e2d24c7417764f20038
meghnaroyal <manthurimeghana@gmail.com>
2025-03-13 22:09:06 +0530

    updated with small change

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:main/hello.cpp
+
 //File: hello.cpp
 #include <iostream>
 using namespace std;
@@ -6,18 +6,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this modified  \n";
   return 0;
 }
-=======
-//File: hello.cpp
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this i made changes  \n";
-  return 0;
-}
->>>>>>> baecad3 (updated hello with small change):hello.cpp
